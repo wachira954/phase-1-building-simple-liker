c01f463f7333348ed7a398a0c5023b2c156bbe8b
wachira <wachiraa954@gmail.com>
2022-06-30 12:56:34 +0300

    building simple liker assgn done

--- main.js
+++ main.js
@@ -4,7 +4,41 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const modal = document.querySelector("#modal");
+modal.classList.add("hidden");
 
+
+const click = (event) =>{
+  const heart = event.target
+  heart.innerText = EMPTY_HEART
+  mimicServerCall()
+    .then( res => {
+      if(heart.innerText === EMPTY_HEART){
+        heart.innerText = FULL_HEART
+        heart.classList.add("activated-heart")
+      }
+      else{
+        heart.innerText = EMPTY_HEART
+        heart.classList.remove("activated-heart")
+      }
+      
+      
+    })
+    .catch(err => {
+      modal.classList.remove("hidden");
+      modal.querySelector("#modal-message").textContent = err;
+      setTimeout( () => {
+        modal.classList.add("hidden")
+      }, 3000)
+    }) 
+   
+}
+
+
+const hearts = document.querySelectorAll(".like-glyph");
+hearts.forEach( heart => {
+  heart.addEventListener("click", click)
+})
 
 
 //------------------------------------------------------------------------------
